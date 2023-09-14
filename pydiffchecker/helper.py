6e5c3d7f07ad651202328aa41682938e01319bfd
Pőcze Bence <bence@pocze.ch>
2023-09-14 19:27:32 +0200

    make subprocess_readlines more robust

--- pydiffchecker/helper.py
+++ pydiffchecker/helper.py
@@ -3,14 +3,13 @@ from typing import Iterator
 
 
 def subprocess_readlines(cmd, cwd=None) -> Iterator[str]:
-    process = subprocess.Popen(cmd, cwd=cwd, stdout=subprocess.PIPE)
+    process = subprocess.Popen(cmd, cwd=cwd, stdout=subprocess.PIPE, text=True)
 
     for line in process.stdout:
-        line = line.decode().rstrip(r'\n')
-
+        line = line.rstrip('\n')
         yield line
 
-    process.wait()
+    process.communicate()
 
     if process.returncode != 0:
         raise subprocess.CalledProcessError(process.returncode, cmd)
